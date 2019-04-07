18ba02aed71ec7f377da6fcd80ff1ba3d8632063
sskender <sskender@users.noreply.github.com>
2019-04-07 14:15:06 +0200

    Basic stopwatch functionality implemented

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,35 +1,65 @@
 package com.example.stopwatch
 
-import android.support.v7.app.AppCompatActivity
+import android.os.AsyncTask
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var numberOfSeconds = 0
+    private var stopwatchThread: AsyncTask<Unit, Unit, Unit>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         startButton.setOnClickListener {
-
+            stopwatchThread = DoInBackground()
+            stopwatchThread?.execute()
         }
 
         pauseButton.setOnClickListener {
-
-        }
-
-        stopButton.setOnClickListener {
-
+            stopwatchThread?.cancel(true)
         }
 
         resetButton.setOnClickListener {
-
+            stopwatchThread?.cancel(true)
+            resetStopwatch()
         }
 
     }
 
+    private fun resetStopwatch() {
+        numberOfSeconds = 0
+        showProgressTextView.text = numberOfSeconds.toString()
+    }
+
     override fun onDestroy() {
+        stopwatchThread?.cancel(true)
         super.onDestroy()
+    }
+
+    inner class DoInBackground : AsyncTask<Unit, Unit, Unit>() {
+
+        override fun onProgressUpdate(vararg values: Unit?) {
+            super.onProgressUpdate(*values)
+
+            numberOfSeconds++
+            showProgressTextView.text = numberOfSeconds.toString()
+        }
+
+        override fun doInBackground(vararg params: Unit?) {
+            while (true) {
+                if (this.isCancelled) {
+                    break
+                }
+
+                Thread.sleep(1000)
+                this.publishProgress()
+            }
+        }
+
     }
 
 }
