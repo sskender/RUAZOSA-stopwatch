0618424bd972c634efc4bdfa100a02b7257f6fcf
sskender <sskender@users.noreply.github.com>
2019-04-07 13:11:00 +0200

    Button listeners created

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.stopwatch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton.setOnClickListener {
+
+        }
+
+        pauseButton.setOnClickListener {
+
+        }
+
+        stopButton.setOnClickListener {
+
+        }
+
+        resetButton.setOnClickListener {
+
+        }
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
 }
